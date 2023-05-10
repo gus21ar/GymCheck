baf969df842796bd71f532d27bd3372691a1167d
Gustavo Vicente Delponte <gus21ar@hotmail.com>
2023-05-09 23:07:16 -0300

    Agregar archivos de proyecto.

---
+++ GymDBData/Repositorio/CClienteRepo.cs
@@ -0,0 +1,109 @@
+﻿using Servicios.Data;
+using Servicios.Repositorio;
+using GymDBData.Mapeadores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GymDBData.Entidades;
+
+namespace GymDBData.Repositorio
+{
+	public class CClienteRepo : IClienteRepo
+	{
+		public bool Agregar(ICliente cliente)
+		{
+			try
+			{
+				using (var db = new GymDbContext())
+				{
+					var clienteNuevo = new Cliente();
+					ClienteMaper.Map(cliente, clienteNuevo);
+					db.Clientes.Add(clienteNuevo);
+					db.SaveChanges();
+					return true;
+				}
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+		}
+
+		public bool HayClientes(int dni)
+		{
+			bool hayClientes = false;
+			try
+			{
+				using (var db = new GymDbContext())
+				{
+					var cliente = db.Clientes.Where(c => c.Dni == dni.ToString()).FirstOrDefault();					
+						hayClientes = cliente != null;					
+				}
+			}
+			catch
+			{
+				hayClientes = false;
+			}
+			return hayClientes;
+		}
+
+		public int ObtenerIdCliente(int dni)
+		{
+			int idCliente = 0;
+			using (var db = new GymDbContext())
+			{
+				var cliente = db.Clientes.Where(c => c.Dni == dni.ToString()).FirstOrDefault();
+				if (cliente != null)
+				{
+					idCliente = cliente.Id;
+				}
+				
+			}
+			return idCliente;
+		}
+
+		public string ObtenerNombreCompleto(int id)
+		{
+			string nombreCompleto = "";
+			using (var db = new GymDbContext())
+			{
+				var cliente = db.Clientes.Where(c => c.Id == id).FirstOrDefault();
+				if (cliente != null)
+				{
+					nombreCompleto = cliente.Nombre + " " + cliente.Apellido;
+				}
+			}
+			return nombreCompleto;
+		}
+
+		public bool AgregarRedes(int dni, List<IUsuariosRedes> listaRedes)
+		{
+			bool respuesta = false;
+			try
+			{
+				using (var db = new GymDbContext())
+				{
+					var cliente = db.Clientes.Where(c => c.Dni == dni.ToString()).FirstOrDefault();
+					if (cliente != null)
+					{
+						foreach (var red in listaRedes)
+						{
+							var redNueva = new UsuarioRedCliente();
+							redNueva.IdCliente = cliente.Id;
+							redNueva.IdRed = db.RedesSociales.First(x => x.NombreRed == red.Red).Id;
+							redNueva.Usuario = red.Usuario;
+							db.UsuarioRedClientes.Add(redNueva);
+						}
+						db.SaveChanges();
+						return true;
+					}					
+				}
+			}
+			catch { }
+			return respuesta;
+		}
+	}
+}
+
---
+++ Servicios/Repositorio/IClienteRepo.cs
@@ -0,0 +1,14 @@
+﻿
+using Servicios.Data;
+
+namespace Servicios.Repositorio
+{
+	public interface IClienteRepo
+	{
+		bool Agregar (ICliente cliente);
+		bool HayClientes (int dni);
+		int ObtenerIdCliente (int dni);
+		string ObtenerNombreCompleto (int id);
+		bool AgregarRedes (int dni,List<IUsuariosRedes> listaRedes);
+	}
+}
